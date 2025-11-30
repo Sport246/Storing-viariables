cb7d6ae0792e8568a3d9fd76ad5c5670e23a009f
Sport246 <itsnihanth.cool@gmail.com>
2025-11-30 10:10:01 +0100

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,10 @@
+t = 04.1982
+e = 11.1968
+a = 08.2003
+s = 01.1965
+b = 08.2008
+print("Tom is born on", t)
+print("Ben is born on", b)
+print("Steve is born on", s)
+print("Adam is born on", a)
+print("Emma is born on", e)
